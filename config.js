bf2301d61ba6a54f61d69cb65e0e3f23164498d6
hassan6262 <treeeuk68@gmail.com>
2025-01-02 00:30:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=6Y0kUCCJ#78-yrNVdSHL637sLXofUht_7W54v2Sqa4kMKnvi1ZVQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=bNViGajQ#NoGIKue6U7kGweVN0X3cFrIoWD5WznKved0Q5bVKf-8' : process.env.SESSION_ID,
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'hassan6262': process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
